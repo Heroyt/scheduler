b671f55787410aedc9e84d2f15dfa321b4bef873
Tomáš Vojík <vojik@wboy.cz>
2024-06-07 15:46:35 +0200

    Upgraded dependencies

--- tests/Doubles/TestLockFactory.php
+++ tests/Doubles/TestLockFactory.php
@@ -1,29 +1,29 @@
-<?php declare(strict_types = 1);
+<?php
+declare(strict_types=1);
 
 namespace Tests\Orisai\Scheduler\Doubles;
 
 use Symfony\Component\Lock\Key;
 use Symfony\Component\Lock\LockFactory;
-use Symfony\Component\Lock\LockInterface;
 use Symfony\Component\Lock\PersistingStoreInterface;
+use Symfony\Component\Lock\SharedLockInterface;
 
 final class TestLockFactory extends LockFactory
 {
 
 	private bool $autoRelease;
 
-	public function __construct(PersistingStoreInterface $store, bool $autoRelease)
-	{
+	public function __construct(PersistingStoreInterface $store, bool $autoRelease) {
 		parent::__construct($store);
 		$this->autoRelease = $autoRelease;
 	}
 
-	public function createLock(string $resource, ?float $ttl = 300.0, ?bool $autoRelease = null): LockInterface
+	public function createLock(string $resource, ?float $ttl = 300.0, ?bool  $autoRelease = null) : SharedLockInterface
 	{
 		return parent::createLock($resource, $ttl, $autoRelease ?? $this->autoRelease);
 	}
 
-	public function createLockFromKey(Key $key, ?float $ttl = 300.0, ?bool $autoRelease = null): LockInterface
+	public function createLockFromKey(Key $key, ?float $ttl = 300.0, ?bool $autoRelease = null) : SharedLockInterface
 	{
 		return parent::createLockFromKey($key, $ttl, $autoRelease ?? $this->autoRelease);
 	}
